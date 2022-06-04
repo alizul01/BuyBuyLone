0f5636ab74f225c4fbb16d9874e71ae92e1d3aee
Ali Zulfikar <alizulfikar032@gmail.com>
2022-06-04 18:26:37 +0700

    Install: Webpack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./**/*.html', './assets/js/**/*.js'],
+    theme: {
+        extend: {
+            screens: {
+                '2xl': '1320px',
+                '3xl': '1440px',
+                '4xl': '1600px',
+                '5xl': '1920px',
+            }
+        },
+    },
+    plugins: [],
 }
